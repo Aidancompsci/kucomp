d7768e669e9aa1e7ab5aa90c8019752182459fc3
MrKrabsCoding <89555978+MrKrabsCoding@users.noreply.github.com>
2024-09-22 10:36:47 -0500

    just getting it in

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+struct WeatherData {
+    date: String,
+    weather_code: f32,
+    temperature_max: f32,
+    temperature_min: f32,
+    precipitation_sum: f32,
+    wind_speed_max: f32,
+    precipitation_probability_max: f32
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
